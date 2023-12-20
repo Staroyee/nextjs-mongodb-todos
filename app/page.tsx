0320fa29ecbdd8f5598eb46ae39868364998366d
Staroyee <danmasefield@gmail.com>
2023-12-20 16:28:49 +1030

    .env fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import AddTodo from "@/components/shared/AddTodo";
+
 const Home = () => {
-  return <div>page</div>;
+  return (
+  <>
+  <div className="w-screen py-20 flex justify-center flex-col items-center  ">
+    <span className="text-3xl font-extrabold">To-Do-App</span>
+    <h1 className="text-3xl font-extrabold uppercase mb-5">Next.js 14</h1>
+    <span className="text-orange-700 ml-2">Server Actions</span>
+    <div className="flex justify-center flex-col item-center width-[1000px]"><AddTodo /></div>
+  </div>
+  </>
+    )
 };
 
 export default Home;
---
+++ components/shared/AddTodo.tsx
@@ -0,0 +1,14 @@
+import Form from "../ui/Form"
+import Input from "../ui/Input"
+import Button from "../ui/Button"
+
+const AddTodo = () => {
+  return (
+    <Form>
+        <div className="flex"></div>
+    </Form>
+    
+  )
+}
+
+export default AddTodo
